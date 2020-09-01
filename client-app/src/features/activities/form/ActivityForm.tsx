e5c206950876a86230a733e3b871634aba7498e7
ydre sso <ydre@outlook.fr>
2020-09-01 19:06:38 +0200

    Mobx refacto client side / store instead of state

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,24 +1,20 @@
-import React, {useState, FormEvent} from 'react';
+import React, {useState, FormEvent, useContext} from 'react';
 import {  Segment, Form, Button } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import {v4 as uuid} from 'uuid';
+import ActivityStore from '../../../app/stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 interface IProps{
-  setEditMode: (editMode: boolean) => void;
   activity: IActivity;
-  createActivity: (activity: IActivity) => void;
-  editActivity: (activity: IActivity) => void;
-  submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
-    setEditMode, 
-    activity: initialFormState,
-    createActivity,
-    editActivity,
-    submitting
-   }) => {
 
+    activity: initialFormState,
+   }) => {
+  const activityStore = useContext(ActivityStore);
+  const {createActivity, editActivity, submitting, cancelFormOpen} = activityStore;
   const initalizeForm = () => {
     if(initialFormState) {
       return initialFormState
@@ -53,6 +49,7 @@ const ActivityForm: React.FC<IProps> = ({
     const {name, value} = event.currentTarget;
     setActivity({...activity, [name]: value});
   }
+
 
   return(
     <Segment clearing>
@@ -89,10 +86,10 @@ const ActivityForm: React.FC<IProps> = ({
           placeholder='Venue' 
           value={activity.venue}/>
         <Button loading={submitting} floated='right' positive type='submit' content='Submit'/>
-        <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel'/>
+        <Button onClick={cancelFormOpen} floated='right' type='button' content='Cancel'/>
       </Form>
     </Segment>
   );
 }
 
-export default ActivityForm;
+export default observer(ActivityForm);
