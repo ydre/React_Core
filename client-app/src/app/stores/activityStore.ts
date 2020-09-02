697a5dcd98fa717158e25912660fa5df20349d94
ydre sso <ydre@outlook.fr>
2020-09-02 13:30:17 +0200

    Client Routing

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -7,10 +7,8 @@ configure({enforceActions: 'always'});
 
 class ActivityStore {
   @observable activityRegistry = new Map();
-  @observable activities: IActivity[] = [];
   @observable loadingInitial = false;
-  @observable selectedActivity: IActivity | undefined;
-  @observable editMode = false;
+  @observable activity: IActivity | null = null;
   @observable submitting = false;
   @observable target = '';
 
@@ -40,13 +38,41 @@ class ActivityStore {
     }
   };
 
+  @action loadActivity = async (id: string) => {
+    let activity = this.getActivity(id);
+    if(activity) {
+      this.activity = activity;
+    }else{
+      this.loadingInitial = true;
+      try{
+        activity = await agent.Activities.details(id);
+        runInAction('getting activity',() => {
+          this.activity = activity;
+          this.loadingInitial = false;
+        })
+      }catch(error){
+        runInAction('get activity error', () => {
+          this.loadingInitial = false;
+        })
+        console.log(error)
+      }
+    }
+  };
+
+  @action clearActivity = () => {
+    this.activity = null;
+  }
+
+  getActivity = (id: string) => {
+    return this.activityRegistry.get(id);
+  }
+
   @action createActivity = async (activity: IActivity) => {
     this.submitting = true;
     try {
       await agent.Activities.create(activity);
       runInAction('creating activity', () => {
         this.activityRegistry.set(activity.id, activity);
-        this.editMode = false;
         this.submitting = false;
       })
     } catch(error){
@@ -63,8 +89,7 @@ class ActivityStore {
       await agent.Activities.update(activity);
       runInAction('editing an activity', () => {
         this.activityRegistry.set(activity.id, activity);
-        this.selectedActivity = activity;
-        this.editMode = false;
+        this.activity = activity;
         this.submitting = false;
       })
     }catch(error){
@@ -73,29 +98,6 @@ class ActivityStore {
       })
       console.log(error);
     }
-  }
-
-  @action openEditForm = (id: string) => {
-    this.selectedActivity = this.activityRegistry.get(id);
-    this.editMode = true;
-  }
-
-  @action cancelSelectedActivity = () => {
-    this.selectedActivity = undefined;
-  }
-
-  @action cancelFormOpen = () => {
-    this.editMode = false;
-  }
-
-  @action openCreateForm = () => {
-    this.editMode = true;
-    this.selectedActivity = undefined;
-  }
-   
-  @action selectActivity = (id: string) => {
-    this.selectedActivity = this.activityRegistry.get(id);
-    this.editMode = false;
   }
 
   @action deleteActivity = async (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 ReactDOM.render(
-    <App />,
-
+  <BrowserRouter>
+  <ScrollToTop> 
+    <App />
+  </ScrollToTop>
+   
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
